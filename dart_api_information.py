fe6e7ccfa558253e02dc2cc632b18f94ba74b12e
baehaejin <bae05030@gmail.com>
2020-08-24 10:58:31 +0900

    add api key and corp code url

---
+++ dart_api_information.py
@@ -0,0 +1,24 @@
+class DartKey:
+    """
+    dart key class
+    """
+    @staticmethod
+    def get_key():
+        __privateApi_key = 'input your api key'
+        return __privateApi_key
+
+
+class DartUrl:
+    """
+    dart api urls
+    """
+    @staticmethod
+    def get_corp_code_url():
+        # this url can corp_code by stock_code or corp_name
+        __privateGetCorpCodeUrl = "https://opendart.fss.or.kr/api/corpCode.xml"
+        """
+        you need to add
+        : ?crtfc_key=your own api key
+        :return: this return url that you can get corp_code
+        """
+        return __privateGetCorpCodeUrl
