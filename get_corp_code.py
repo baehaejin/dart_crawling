03ad76c504535328b178ca4ae5d0dd958912f982
baehaejin <bae05030@gmail.com>
2020-08-24 10:36:42 +0900

    modify get_corp_code

--- get_corp_code.py
+++ get_corp_code.py
@@ -1,4 +1,3 @@
-import requests
 from urllib.request import urlopen
 from zipfile import ZipFile
 from io import BytesIO
@@ -6,34 +5,43 @@ from bs4 import BeautifulSoup
 import dart_api_information
 
 
-def find_corp_code(corp_name, stock_code):
+def get_corp_code(corp_name, stock_code):
+    """
+    This method return corp_code by cor_name or stock_code
+    if you input corp_name or stock_code this method will find corporation about your input
+    if finding corporation is not one this method will see you about list
+    :param corp_name: name of corporation
+    :param stock_code: stock code of corporation
+    :return: return corporation code
+    """
     # get url and api_key from dart_api_information
-    key_class = dart_api_information.DartKey()
-    url_info = dart_api_information.DartUrl()
-    api_key = key_class.get_key()
-    url = url_info.get_corp_code_url()
-    url = url + "?crtfc_key=" + api_key
+    api_key = dart_api_information.DartKey.get_key()
+    api_url = dart_api_information.DartUrl.get_corp_code_url()
+    url = api_url + "?crtfc_key=" + api_key
 
     corp_code = ""
     corp_name_list = []
 
     # temp
-    corp_name = "삼천"
     stock_code = "000250"
+
+    if corp_name == "":
+        corp_name = "do not search by this"
+    elif stock_code == "" and corp_name == "":
+        print("해당 기업의 정보가 존재하지 않습니다.")
+        return -1
 
     # The bytes of the received resp are accumulated in the buffer and the zip file is loaded.
     openurl = urlopen(url)
-    html = requests.get(url)
     with ZipFile(BytesIO(openurl.read())) as zf:
         file_list = zf.namelist()
         while len(file_list) > 0:
             file_name = file_list.pop()
             # zipfile decode because it is bytes code
-            corpCode = zf.open(file_name).read().decode()
-            # print(corpCode)
+            zf_open = zf.open(file_name).read().decode()
             break
 
-    soup = BeautifulSoup(corpCode, 'html.parser')
+    soup = BeautifulSoup(zf_open, 'html.parser')
     res = soup.find_all('list')
 
     # get corp code by input stock_code and corp_name
@@ -47,7 +55,15 @@ def find_corp_code(corp_name, stock_code):
                 corp_code = n.find('corp_code').get_text()
 
     # find one corp_code
-    if len(corp_name_list) != 1:
+    if len(corp_name_list) == 0:
+        print("해당 기업의 정보가 존재하지 않습니다.")
+        return -1
+    elif len(corp_name_list) == 1:
+        corp_name = corp_name_list[0].find('corp_name').get_text()
+        print("선택하신 종목 : " + corp_name)
+        corp_code = corp_name_list[0].find('corp_code').get_text()
+        stock_code = corp_name_list[0].find('stock_code').get_text()
+    elif len(corp_name_list) != 1:
         for i, name_list in enumerate(corp_name_list, 1):
             print_corp_name = name_list.find('corp_name').get_text()
             print(str(i) + ". : " + print_corp_name)
@@ -64,9 +80,4 @@ def find_corp_code(corp_name, stock_code):
                 stock_code = name_list.find('stock_code').get_text()
                 break
 
-    print(corp_code)
-    print(corp_name)
-    print(stock_code)
-
     return corp_code
-
