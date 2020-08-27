e30c77ed17bc8c6f27ddecd3335d15a979d4bcf1
baehaejin <bae05030@gmail.com>
2020-08-27 13:42:17 +0900

    modify get document

--- get_document.py
+++ get_document.py
@@ -5,24 +5,27 @@ from bs4 import BeautifulSoup
 import dart_api_information
 
 
-def get_document(rcept_no):
+def get_document(rcept_no, report_nm):
+    print()
+    print(report_nm + "검색중입니다. 잠시만 기다려 주세요")
 
     document = []
     api_key = dart_api_information.DartKey.get_key()
     api_url = dart_api_information.DartUrl.get_document_url()
     url = api_url + "?crtfc_key=" + api_key + "&rcept_no=" + rcept_no
-    print(url)
+
     open_url = urlopen(url)
     with ZipFile(BytesIO(open_url.read())) as zf:
         file_list = zf.namelist()
-        print(file_list)
+
         while len(file_list) > 0:
             file_name = file_list.pop()
             # zipfile decode because it is bytes code
             zf_open = zf.open(file_name).read()
+
             break
     soup = BeautifulSoup(zf_open, 'html.parser')
-    print(soup)
+
     res = soup.find_all("section-2")
 
     for n in res:
