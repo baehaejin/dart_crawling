2594d8dfa559bbafee07723382ce091cab4a1d31
배해진 <bae05030@ajou.ac.kr>
2020-09-08 21:23:18 +0900

    resolve error

--- export_excel.py
+++ export_excel.py
@@ -1,11 +1,15 @@
 from openpyxl import Workbook
-
+import get_document
 
 def export_excel(corp_name, document, report_nm):
-
+    if type(document) == " ":
+        print()
+        print(report_nm + "의 내용은 현재 삭제되었습니다")
+        print()
+        return 0
     wb = Workbook()
     xlsx = wb.active
-    title_name = corp_name + "-" + report_nm
+    title_name = corp_name + "-" + report_nm.replace("[", "").replace("]", '-')
     xlsx.title = title_name
     table = document[0].find_all('table')
     title = []
@@ -32,3 +36,4 @@ def export_excel(corp_name, document, report_nm):
         row_index = row_index + 1
     wb.save(filename=title_name + '.xlsx')
 
+# export_excel('삼천당제약',get_document.get_document('20180618000112','[기재정정]사업보고서 (2017.12)'),'[기재정정]사업보고서 (2017.12)')
